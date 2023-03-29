14dc0483915843bde5fc6e797f3b3ae6b6868cf5
Your Name <121116261+farhangold@users.noreply.github.com>
2023-03-29 17:14:25 +0700

    Menambahkan SayaTubeVideo dan SayaTubeUser

---
+++ SayaTubeUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302213073.jurnalMod6_1302213073
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubesVideo> uploadVideos;
+        private String Username;
+
+        public SayaTubeUser(String Username)
+        {
+            this.Username = Username;
+            id = new Random().Next(10000, 99999);
+
+        }
+
+        public void GetTotalVideoPlayCount(int id)
+        {
+            int total = 0;
+            for(int i = 0; i< uploadVideos.Length; i++)
+            {
+                total += uploadVideos.Length;
+            }
+            return total;
+        }
+        public void AddVideo(SayaTubeUser video)
+        {
+            AddVideo.uploadVideos(video);
+        }
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine("User"+ Username);
+            for (int i = 0; i < uploadVideos.Count || i<8; i++)
+            {
+                Console.WriteLine("Video ");
+            }
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302213073.jurnalMod6_1302213073
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        public String title;
+        public int playCount;
+
+
+    }
+    
+    public SayaTubeVideo(String title)
+    {
+        if (title == null || title.Length;title++)
+
+        
+    }
+    public void IncreasePlayCount(int count)
+    {
+        if(count> 100000)
+        {
+
+        }
+    }
+    public void PrintVideoDetail()
+    {
+        Console.WriteLine("Video ID: {0}", this.id);
+        Console.WriteLine("Title : {0}", this.title);
+        Console.WriteLine("Playcount : {0}", this.playCount);
+    }
+}
